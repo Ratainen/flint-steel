091d4c65aec51609faca572e5938c8dd29e007a2
JunkyDeveloper <junkydeveloper@gmail.com>
2026-03-13 21:29:40 +0100

    fix air

--- src/convert.rs
+++ src/convert.rs
@@ -44,7 +44,11 @@ pub fn state_id_to_block(state_id: BlockStateId) -> Block {
         return Block::new("minecraft:air");
     };
 
-    let id = format!("minecraft:{}", block.key.path);
+    let mut id = format!("minecraft:{}", block.key.path);
+
+    if block.key.path == "void_air" || block.key.path == "cave_air" {
+        id = format!("minecraft:{}", "air");
+    }
 
     // Get properties from the registry
     let props = REGISTRY.blocks.get_properties(state_id);
